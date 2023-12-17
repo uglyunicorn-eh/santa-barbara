7cf487d001e74e544cf493fd35aa948b4d77f9af
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-16 19:43:27 -0800

    Remove unnecessary import and add mobile styling to footer and welcome box

--- src/components/hoc/WelcomeBox.tsx
+++ src/components/hoc/WelcomeBox.tsx
@@ -5,8 +5,6 @@ import { LoginBox } from "src/components/hoc/LoginBox";
 import { StartPage } from "src/components/hoc/StartPage";
 import { useCurrentUser } from "src/components/hooks";
 
-import "src/styles/welcome.scss";
-
 export const WelcomeBox = () => {
   const { user } = useCurrentUser();
 
