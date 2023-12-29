a9ca8f2bb011e2f988fdf7045800e241f12ca0f2
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-29 00:18:50 -0800

    Update profile URL in Footer component

--- src/components/hoc/Footer.tsx
+++ src/components/hoc/Footer.tsx
@@ -40,7 +40,7 @@ export const Footer = ({ children, noUser, noHome, profileAsLink }: Props) => {
     () => [
       // { key: "test-enter", label: "Test Enter", url: "/enter/eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsInQiOiJFbnRlclJlcXVlc3QiLCJfIjoiMzkwMjlhNDg2ZGZjZmVjMSJ9.eyJlbWFpbCI6InBhc2hrYS5yZXpuaWtvdkBnbWFpbC5jb20iLCJleHAiOjE3MDM4MDc2NzUsImlzcyI6ImFwaS5nbm9taWsubWUiLCJpYXQiOjE3MDM4MDczNzV9.SNntscTrICTAYv_oVDTiJNsmc0tzLHQKXuHVVRrhpBzX12Ir8aUWoCQ0oOxRhKRLIJKO5eBMlQCI8J_HSFTiqz3VAUiAEGHdx-tfgkx7W0TufE2LTltm2x0MXsCWTJbEcnCUppoPBL4BPEUFJuF0C9Z7QDFSVjizOEPwnayCjj4XQR6B3NhUdEojtqLq_EoGWfltaohI0l5QKf20_CQkCpX80yBs8V64Icej4pJ_Cuy-KwOYMrP0pm8rV_wlXHfytY4XatZVlsd24z3BSqcwrUsP_F4By7ZPXMalFtcOPWJMhOp-CACw62rfiMOaxYgKYVFUHmx7PvgtHxcWb02OCQ", asLink: profileAsLink },
       !noHome ? { key: "home", label: "Home", url: "/" } : null,
-      (!noUser && profile) ? { key: "profile", label: "My Profile", url: "/profile/", asLink: profileAsLink } : null,
+      (!noUser && profile) ? { key: "profile", label: "My Profile", url: "/profile/history/", asLink: profileAsLink } : null,
       (!noUser && profile) ? { key: "sign-out", label: "Sign Out", url: "/", onClick: onSignOutClick } : null,
       { key: "hot-it-works", label: "How this works?", url: "/how-it-works/" },
       { key: "privacy", label: "Privacy Policy", url: "/privacy/" },
