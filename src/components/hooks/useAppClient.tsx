ba2f07a29929bf22b1c9b4980c5564a5745d225c
Pavel Reznikov <pashka.reznikov@gmail.com>
2023-12-29 00:45:57 -0800

    Add closePartyApi mutation to useAppClient hook

--- src/components/hooks/useAppClient.tsx
+++ src/components/hooks/useAppClient.tsx
@@ -25,6 +25,10 @@ export type LeavePartyInput = {
   party: string;
 }
 
+export type ClosePartyInput = {
+  party: string;
+}
+
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const useAppClient = () => {
@@ -100,6 +104,22 @@ export const useAppClient = () => {
     `
   );
 
+  const [closePartyApi, { error: closePartyError }] = useMutation<any, { input: LeavePartyInput }>(
+    gql`
+      mutation Mutation($input: ClosePartyInput!) {
+        parties {
+          closeParty(input: $input) {
+            status
+            userErrors {
+              fieldName
+              messages
+            }
+          }
+        }
+      }
+    `
+  );
+
   const [enterRequestApi, { error: enterRequestError }] = useMutation(
     gql`
       mutation EnterRequest($input: EnterRequestInput!) {
@@ -118,6 +138,7 @@ export const useAppClient = () => {
 
   React.useEffect(() => { joinPartyError?.message && error(joinPartyError?.message); }, [joinPartyError?.message]);
   React.useEffect(() => { leavePartyError?.message && error(leavePartyError?.message); }, [leavePartyError?.message]);
+  React.useEffect(() => { closePartyError?.message && error(closePartyError?.message); }, [closePartyError?.message]);
   React.useEffect(() => { enterRequestError?.message && error(enterRequestError?.message); }, [enterRequestError?.message]);
 
   const getParty = React.useCallback(
@@ -179,13 +200,16 @@ export const useAppClient = () => {
   );
 
   const closeParty = React.useCallback(
-    async (code: string): Promise<boolean> => {
-      console.log("API closeParty", { code });
-      await sleep(500);
-      error("Hm... we cannot this party. Please check the code and try again...");
-      return false;
+    async (input: ClosePartyInput): Promise<boolean> => {
+      const { data } = await closePartyApi({ variables: { input } });
+      if (data?.parties.closeParty.status === "error") {
+        error(data?.parties.closeParty.userErrors[0].messages[0]);
+        return false;
+      }
+      return true;
     },
     [
+      leavePartyApi,
       error,
     ],
   );
